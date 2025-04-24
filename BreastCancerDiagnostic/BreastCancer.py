60074e7de35079c67d153bdfe10bf1a716b6953e
Eduarda Medeiros <165853048+duhdah@users.noreply.github.com>
2025-04-23 22:21:13 -0300

    Update BreastCancer.py

--- BreastCancerDiagnostic/BreastCancer.py
+++ BreastCancerDiagnostic/BreastCancer.py
@@ -9,6 +9,8 @@ Original file is located at
 ## Início
 """
 
+"""Link do Kaggle: https://www.kaggle.com/datasets/uciml/breast-cancer-wisconsin-data"""
+
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import KFold
